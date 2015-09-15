fc9eaf537c3ff267ac56a2da0782e9b875aa0e04
MSpelman <michaelspelman@Michael-Spelmans-iMac.local>
2015-09-15 18:19:42 -0500

    Improvements to products index
    Adds the following functionality to products#index:
    - Filter by product type
    - Live search on name
    - Sort by columns

--- test/models/product_type_test.rb
+++ test/models/product_type_test.rb
@@ -7,4 +7,21 @@ class ProductTypeTest < ActiveSupport::TestCase
     assert_nothing_raised { ProductType.find(product_type_id) }
   end
 
+  test "should not create product type with duplicate name" do
+    product_type = ProductType.new
+    product_type.name = "Whiskey"
+    assert !product_type.valid?
+    assert product_type.errors[:name].any?
+    assert_equal ["has already been taken*"], product_type.errors[:name]
+    assert !product_type.save
+  end
+
+  test "should not create product type with no name" do
+    product_type = ProductType.new
+    assert !product_type.valid?
+    assert product_type.errors[:name].any?
+    assert_equal ["can't be blank*"], product_type.errors[:name]
+    assert !product_type.save
+  end
+
 end
